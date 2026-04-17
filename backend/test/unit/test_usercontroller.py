900616fefea04854f1edb05ae36b295c4e7b262a
Elfving <sebastian.elfving@hotmail.com>
2026-04-17 18:51:36 +0200

    fixed some parts of the code

--- backend/test/unit/test_usercontroller.py
+++ backend/test/unit/test_usercontroller.py
@@ -17,7 +17,6 @@ def test_get_user_by_email_invalid(usercontroller):
     email = "invalidemail"
     with pytest.raises(ValueError, match='Error: invalid email address'):
         usercontroller.get_user_by_email(email)
-    assert True
 
 #   
 #   This test validates if email is valid
@@ -29,7 +28,7 @@ def test_get_user_by_email_valid(usercontroller):
     assert usercontroller.get_user_by_email(email) == {'email': email}
 
 #
-# Test if print statement is correctly printed when multiple users are found with the same email address
+# Test if pirnt statement is correctly printed when multiple users are found with the same email address
 #
 @pytest.mark.unit
 def test_get_user_by_email_multiple(usercontroller, capsys):
@@ -51,15 +50,14 @@ def test_get_return_user_by_email_multiple(usercontroller):
     email = "test@test.com"
     usercontroller.dao.find.return_value = [{'email': email}, {'email': email}]
 
-
     usercontroller.get_user_by_email(email)
     assert usercontroller.get_user_by_email(email) == {'email': email}
 
 
 #
 #  This test validates if email is valid but no user is found with that email
-#  address will raise a value error because we return the first element of the list 
-#  even if the user doesn't exist, So this is a bug 
+#  address will raise a value error becuase we return the first element of the list even if the user dosent exist 
+#  So this is a bug 
 #
 
 @pytest.mark.unit
@@ -68,5 +66,17 @@ def test_get_user_by_email_no_user(usercontroller):
     with pytest.raises(IndexError, match="list index out of range"):
         usercontroller.dao.find.return_value = []
         usercontroller.get_user_by_email(email)
-    assert True
 
+
+
+#
+# This test test the code when there is an error in the database operation
+# Dosent  
+#
+
+@pytest.mark.unit
+def test_get_user_by_email_db_error(usercontroller):
+    email = "test@test.com"
+    usercontroller.dao.find.side_effect = Exception("Database error")
+    with pytest.raises(Exception):
+        usercontroller.get_user_by_email(email)
